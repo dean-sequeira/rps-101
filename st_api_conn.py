0ac2356dbe2bd704ccb673caaaf396ab2e5f4b82
Dean Sequeira <dean@newshare.co.za>
2023-08-01 19:15:18 +1200

    Refactor class

--- st_api_conn.py
+++ st_api_conn.py
@@ -5,31 +5,36 @@ import requests
 
 class APIConnection(ExperimentalBaseConnection[requests]):
 
-    def _connect(self, route=None, method='get', payload=None) -> requests.get:
-        """Connect to the API and return the response
-        :param route: The API endpoint
-        :param method: The HTTP method
-        :param payload: The payload to send
-        :return: The response
-        """
-        if 'headers' in self._secrets:
-            headers = self._secrets['headers']
-        else:
-            headers = None
-        if method == 'post':
-            return requests.post(f"{self._secrets['url']}{route}", headers=headers, data=payload)
-        else:
-            return requests.get(f"{self._secrets['url']}{route}", headers=headers, data=payload)
+    def _connect(self, route=None, method='get', payload=None):
+        """Connect to the API and return the response.
 
-    def get(self, route=None, method='get', payload=None) -> requests.get:
-        """Connect to the API and return the response using the _connect method
-        :param route: The API endpoint
-        :param method: The HTTP method
-        :param payload: The payload to send
-        :return: The response
+        :param route: The API endpoint.
+        :param method: The HTTP method.
+        :param payload: The payload to send.
+        :return: The response object.
+        """
+        headers = self._secrets.get('headers', None)
+        url = f"{self._secrets['url']}{route}"
+
+        if method.lower() == 'post':
+            response = requests.post(url, headers=headers, data=payload)
+        else:
+            response = requests.get(url, headers=headers, data=payload)
+
+        return response
+
+    def get(self, route=None, method='get', payload=None):
+        """Connect to the API and return the JSON response using the _connect method.
+
+        :param route: The API endpoint.
+        :param method: The HTTP method.
+        :param payload: The payload to send.
+        :return: The JSON response data.
         """
         try:
-            self._connect(route, method, payload).raise_for_status()
+            response = self._connect(route, method, payload)
+            response.raise_for_status()
+            return response.json()
         except requests.exceptions.HTTPError as e:
             st.error(f"Error: {e}")
-        return self._connect(route, method, payload).json()
+            return None
